635174d8b8ed990a3a4dafc39917008cf8c9bd11
Lisa <lisaihde@web.de>
2018-10-31 16:08:39 -0300

    add shooting

---
+++ Assets/Scripts/ShootProjectile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootProjectile : MonoBehaviour {
+
+    public Rigidbody2D projectile;
+
+    void Start () {
+        Shoot(10.0f, 45.0f);
+    }
+
+    public void Shoot(float velocity, float angle)
+    {
+        var shootedProjectile = Instantiate(projectile, projectile.position, Quaternion.identity).GetComponent<Rigidbody2D>();
+        var shootDir = Quaternion.Euler(0, 0, angle) * Vector3.right;
+        shootedProjectile.velocity = shootDir * velocity;
+    }
+
+}
