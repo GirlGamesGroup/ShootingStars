e3b299763d27fc0a743d451299016b008cd3d9ff
Lisa <lisaihde@web.de>
2018-11-01 15:11:55 -0300

    add drag to projectile to stop

--- Assets/Scripts/ShootProjectile.cs
+++ Assets/Scripts/ShootProjectile.cs
@@ -7,14 +7,35 @@ public class ShootProjectile : MonoBehaviour {
     public Rigidbody2D projectile;
 
     void Start() {
-        Shoot(2.0f, 25.0f);
+        Shoot(2.0f, 105.0f);
     }
 
-    public void Shoot(float velocity, float angle)
+    //range of angle is [105|75] degree to be in the area of main screen
+    //range of velocity between [0|3] from device input a
+    public void Shoot(float acceleration, float angle)
     {
+        var velocity = acceleration * 100f;
         var shootedProjectile = Instantiate(projectile, transform.position, transform.rotation).GetComponent<Rigidbody2D>();
         var shootDir = Quaternion.Euler(0, 0, angle) * Vector3.right;
-        shootedProjectile.velocity = shootDir * velocity;
+
+        shootedProjectile.AddForce(shootDir * velocity);
+        StartCoroutine(AddDrag(shootedProjectile, velocity));
+    }
+
+    IEnumerator AddDrag(Rigidbody2D projectile, float velocity)
+    {
+        float current_drag = 0;
+
+        while (current_drag < velocity)
+        {
+            current_drag += Time.deltaTime * (.15f * velocity);
+            projectile.drag = current_drag;
+            yield return null;
+        }
+
+        projectile.velocity = Vector3.zero;
+        projectile.angularVelocity = 0;
+        projectile.drag = 0;
     }
 
 }
